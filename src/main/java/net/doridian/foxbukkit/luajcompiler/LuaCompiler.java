1047ef8bae03ff70fd9df34de019263e545aa75b
Doridian <git@doridian.net>
2023-09-02 23:43:04 -0700

    fix

--- src/main/java/net/doridian/foxbukkit/luajcompiler/LuaCompiler.java
+++ src/main/java/net/doridian/foxbukkit/luajcompiler/LuaCompiler.java
@@ -1,22 +1,23 @@
-/**
- * This file is part of plexus-compiler-luaj.
+/*
+ * plexus-compiler-luaj - ${project.description}
+ * Copyright © ${year} Doridian (git@doridian.net)
  *
- * plexus-compiler-luaj is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * plexus-compiler-luaj is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with plexus-compiler-luaj.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.foxelbox.luajcompiler;
+package net.doridian.foxbukkit.luajcompiler;
 
-import com.foxelbox.luajcompiler.compiler.LuaJC;
+import net.doridian.foxbukkit.luajcompiler.compiler.LuaJC;
 import org.codehaus.plexus.compiler.*;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.lib.jse.JsePlatform;
@@ -58,6 +59,11 @@ public class LuaCompiler extends AbstractCompiler {
     private String stripExtension(String name) {
         int lastDot = name.lastIndexOf('.');
         return name.substring(0, lastDot);
+    }
+
+    @Override
+    public String getCompilerId() {
+        return "luaj";
     }
 
     @Override
